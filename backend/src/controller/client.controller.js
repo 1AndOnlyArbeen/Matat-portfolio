d68c78b390b2435007df001a294614c0f84d524d
Arbeen <arbinbabuthapamagar2002@gmail.com>
2026-04-26 17:34:32 +0545

    Correct typo in deleteClient response message

--- backend/src/controller/client.controller.js
+++ backend/src/controller/client.controller.js
@@ -134,7 +134,7 @@ const deleteClient = asyncHandler(async (req, res) => {
   }
   return res
     .status(200)
-    .json(new apiResponse(200, {}, ' Client details deleteed successfully'));
+    .json(new apiResponse(200, {}, ' Client details deleted successfully'));
 });
 
 export { createClient, getAllClient, editClientDetails, deleteClient };
